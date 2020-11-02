5d20c8aa4c38da2e7b99af1639b93d8bb381a6b5
tiffanishilts <tshilts2@pdx.edu>
2020-11-02 10:22:50 -0800

    made error comments able to toggle

--- fileOperations.sv
+++ fileOperations.sv
@@ -7,34 +7,53 @@
 
 module fileOperations;
 
- timeunit 1ns/1ns;
+timeunit 1ns/1ns;
     
 	initial begin
 
         string file;                                                            // string variable for file name
 
-        int fd_r, clocks, operation, hexAddr;                                   // file descriptor for read operation, cpu clock cycles, operation, hexadecimal address
+        int fd_r, clocks, operation, hexAddr, errorCheck;                       // file descriptor for read operation, cpu clock cycles, operation, hexadecimal address
 
-        if ($value$plusargs ("FILENAME=%s", file))                              // attempt to get file name from command line
-                                                                                // user must specify input as: +FILENAME=filename OR +FILENAME="filename" exactly
-            $display ("Filename is: %s", file);                                 // display file name if retrieved successfully
+        if ($value$plusargs ("ERRORCHECK=%d", errorCheck))                      // attempt to error preference from command line
+                                                                                // user must specify input as: +ERRORCHECK=0/1 exactly; 0 no, 1 yes
+            $display ("Error check preference is: %d", errorCheck);             // display preference if retrieved successfully
         else
-            $display ("File name retrieval unsuccessful");                      // else tell user retrieval was unsuccessful
+            $display ("Error preference retrieval unsuccessful");               // else tell user retrieval was unsuccessful
 
-        fd_r = $fopen(file, "r");                                               // attempt to open file user has specified                           
+                                               
+        if(errorCheck == 1)
+        begin
+            if ($value$plusargs ("FILENAME=%s", file))                              // attempt to get file name from command line
+                                                                                    // user must specify input as: +FILENAME=filename OR +FILENAME="filename" exactly
+                $display ("Filename is: %s", file);                                 // display file name if retrieved successfully
+            else
+                $display ("File name retrieval unsuccessful");                      // else tell user retrieval was unsuccessful
 
-        if (fd_r)                                                               // display error code from file descriptor to determine file operation success
-            $display ("File was opened successfully: %0d", fd_r);               // some negative number will display if success
+            fd_r = $fopen(file, "r");                                               // attempt to open file
+            
+            if (fd_r)                                                               // display error code from file descriptor to determine file operation success
+                $display ("File was opened successfully: %0d", fd_r);               // some negative number will display if success
+            else
+                $display ("File was NOT opened successfully: %0d", fd_r);           // 0 will display if failure
+
+            while ($fscanf (fd_r, "%d %d %x", clocks, operation, hexAddr) == 2)     // continue to read file until conversion failure or eof whichever is first
+                begin
+                    $display("%d %d %x", clocks, operation, hexaddr);               // display each line to ensure proper parsing
+                end
+
+            $fclose(fd_r);
+        end
         else
-            $display ("File was NOT opened successfully: %0d", fd_r);           // 0 will display if failure
+        begin
+            $value$plusargs ("FILENAME=%s", file);                                  // attempt to get file name from command line
 
-        while ($fscanf (fd_r, "%d %d %x", clocks, operation, hexAddr) == 2)     // continue to read file until conversion failure or eof whichever is first
-            begin
-                $display("%d %d %x", clocks, operation, hexaddr);               // display each line to ensure proper parsing
-            end
+            fd_r = $fopen(file, "r");                                               // attempt to open file
 
-        $fclose(fd_r);                                                          // close file                                                 
+            while ($fscanf (fd_r, "%d %d %x", clocks, operation, hexAddr) == 2);    // continue to read file until conversion failure or eof whichever is first
 
+            $fclose(fd_r);
+        end
     end
 
 endmodule
